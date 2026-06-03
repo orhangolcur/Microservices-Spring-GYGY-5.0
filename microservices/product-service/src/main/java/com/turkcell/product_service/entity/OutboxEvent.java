c145023bb67cec5d1ab01c225581a41d195653be
Orhan Gölcür <orhangolcur0@gmail.com>
2026-06-03 20:15:00 +0300

    outbox pattern eklendi

--- microservices/product-service/src/main/java/com/turkcell/product_service/entity/OutboxEvent.java
+++ microservices/product-service/src/main/java/com/turkcell/product_service/entity/OutboxEvent.java
@@ -28,4 +28,86 @@ public class OutboxEvent {
 
     @Enumerated(EnumType.STRING) // numara olarak mı saklayalım yoksa string olarak mı saklayalım
     private OutboxStatus status; // PENDING, SENT, FAILED
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getAggregateType() {
+        return aggregateType;
+    }
+
+    public void setAggregateType(String aggregateType) {
+        this.aggregateType = aggregateType;
+    }
+
+    public String getAggregateId() {
+        return aggregateId;
+    }
+
+    public void setAggregateId(String aggregateId) {
+        this.aggregateId = aggregateId;
+    }
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+
+    public String getPayload() {
+        return payload;
+    }
+
+    public void setPayload(String payload) {
+        this.payload = payload;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public int getRetryCount() {
+        return retryCount;
+    }
+
+    public void setRetryCount(int retryCount) {
+        this.retryCount = retryCount;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getProcessedAt() {
+        return processedAt;
+    }
+
+    public void setProcessedAt(Instant processedAt) {
+        this.processedAt = processedAt;
+    }
+
+    public OutboxStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OutboxStatus status) {
+        this.status = status;
+    }
+
+    
 }
--- microservices/product-service/src/main/java/com/turkcell/product_service/web/controller/ProductController.java
+++ microservices/product-service/src/main/java/com/turkcell/product_service/web/controller/ProductController.java
@@ -1,21 +1,29 @@
 package com.turkcell.product_service.web.controller;
 
+import java.time.Instant;
 import java.util.UUID;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.turkcell.product_service.entity.OutboxEvent;
+import com.turkcell.product_service.entity.OutboxStatus;
 import com.turkcell.product_service.event.TestEvent;
+import com.turkcell.product_service.repository.OutboxRepository;
 
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
 
-    private final StreamBridge streamBridge;
+    private final OutboxRepository outboxRepository;
+    private final ObjectMapper objectMapper;
 
-    public ProductController(StreamBridge streamBridge) {
-        this.streamBridge = streamBridge;
+    public ProductController(OutboxRepository outboxRepository, ObjectMapper objectMapper) {
+        this.outboxRepository = outboxRepository;
+        this.objectMapper = objectMapper;
     }
 
     @GetMapping("/hello")
@@ -25,8 +33,40 @@ public class ProductController {
 
     @GetMapping
     public String sendTestEvent(@RequestParam String message) {
-        var event = new TestEvent(message, UUID.randomUUID());
-        streamBridge.send("testEvent-out-0", event);
+        UUID id = UUID.randomUUID();
+        UUID eventId = UUID.randomUUID();
+        var event = new TestEvent(eventId, message, id);
+
+            // KAFKAYA bir event gidecekse, önce kayıt altına alınacak.
+        // Outbox -> XEvent,XTarihi,XTopic,XPayload
+
+
+        // Daha sonra bir mekanizma bu kayıtları okuyacak ve kafkaya gönderecek.
+        // POLLING ->
+        // POLLING -> Belirli aralıklarla veritabanaına bak, gönderilecek bir event var mı?
+        // Her 20 snde => SElect * from outbox where status = 'PENDING' and retryCount < 3
+        // CDC (Change Data Capture)
+        // Debezium gibi bir mekanizma
+
+        OutboxEvent outboxEvent = new OutboxEvent();
+        outboxEvent.setId(eventId);
+        outboxEvent.setAggregateType("Product");
+        outboxEvent.setAggregateId(id.toString());
+        outboxEvent.setEventType("testEvent");
+        outboxEvent.setPayload(toJson(event));
+        outboxEvent.setStatus(OutboxStatus.PENDING);
+        outboxEvent.setCreatedAt(Instant.now());
+
+        outboxRepository.save(outboxEvent);
+
+        outboxRepository.save(outboxEvent);
+
         return "Başarılı";
     }
+     private String toJson(Object o)
+    {
+        try { return objectMapper.writeValueAsString(o);}
+        catch(Exception e) { throw new RuntimeException(e); }
+    }
 }
+ 
